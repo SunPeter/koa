c9bbaf5b798995a358d940e46604fa309cc853d2
SunPeter <ssd3237649@gmail.com>
2015-07-30 23:53:45 +0800

    中转服务器接入

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var os = require("os")
+var os = require("os");
+var querystring = require("querystring");
 var koa = require("koa");
 var handlebars = require("koa-handlebars");
 var request = require('koa-request');
@@ -7,7 +8,7 @@ var crypto = require('crypto');
 var app = koa();
 var _static = require('koa-static');
 var router = require('koa-router')();
-var appid = "wx98831d7cee9dc881",secret = "34c487c0f12bdf000fab9f836215ada6",url="http://ssd3237649.xicp.net/";
+var appid = "wx98831d7cee9dc881",secret = "34c487c0f12bdf000fab9f836215ada6",url="http://ssd3237649.xicp.net/",token="weixintoken";
 
 app.use(_static('./public'));
 
@@ -16,12 +17,36 @@ app.use(handlebars({
 }));
 app.use(router.routes()).use(router.allowedMethods());
 
-router.get('/', function *(next) {
+router.get('/index', function *(next) {
     var data =yield sign();
     yield this.render("index", data);
-});
+}).get('/',function* (next){
+    var qs =querystring.parse(this.request.querystring);
+    qs.token =token;
+    var signature =qs.signature;
+    var echostr =qs.echostr;
+    delete qs.signature;
+    delete qs.echostr;
+    var keys =Object.keys(qs).sort();
+    var _qs ="";
+    for(var i =0 ;i <keys.length;i++){
+        _qs+=qs[keys[i]];
+    }
+    var _signature =getsignature(_qs);
+    this.body=echostr;
+    
+}).post('/',function* (next){
+    console.log(this.request);
+    this.body ="haha";
+})
 
+function getsignature(qs){
+    var shasum = crypto.createHash('sha1');
+    shasum.update(qs);
 
+    var signature = shasum.digest('hex');
+    return signature;
+}
 function* sign() {
     var options = {
         url: 'https://api.weixin.qq.com/cgi-bin/token',
